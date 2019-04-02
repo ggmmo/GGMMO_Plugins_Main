36936a3f26e3c66710307d44598ea54a2c3f22bd
The_Pale_One <mindcraftlachlan@outlook.com>
2019-04-03 02:43:40 +1100

    - added feature if plugin is loaded and no initial joinspawn is found in the config, create a default at 0 0 0 of the server world

--- adminteleport/src/main/java/ggmmo/adminteleport/GGMMO_AdminTeleport.java
+++ adminteleport/src/main/java/ggmmo/adminteleport/GGMMO_AdminTeleport.java
@@ -5,7 +5,10 @@ import ggmmo.adminteleport.commands.CmdSetWarp;
 import ggmmo.adminteleport.commands.CmdWarp;
 import ggmmo.adminteleport.events.EventJoinTeleport;
 import ggmmo.adminteleport.utils.MessageManager;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Server;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GGMMO_AdminTeleport extends JavaPlugin {
@@ -27,6 +30,12 @@ public class GGMMO_AdminTeleport extends JavaPlugin {
         getCommand("warp").setExecutor(new CmdWarp());
 
         loadConfig();
+
+        if(getConfig().getString("joinspawn") == null){
+            String w = getServer().getWorlds().stream().filter(world -> (!world.getName().contains("the_end")) && !world.getName().contains("nether")).findFirst().get().getName();
+            SetSpawnConfig(w,0,0,0,0,0);
+            MessageManager.consoleInfo("Default world set to: " + w);
+        }
     }
 
     @Override
@@ -36,6 +45,16 @@ public class GGMMO_AdminTeleport extends JavaPlugin {
 
     private void loadConfig() {
         getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+
+    public void SetSpawnConfig(String world, int x, int y, int z, int yaw, int pitch){
+        getConfig().set("joinspawn.world", world);
+        getConfig().set("joinspawn.x", x);
+        getConfig().set("joinspawn.y", y);
+        getConfig().set("joinspawn.z", z);
+        getConfig().set("joinspawn.yaw", yaw);
+        getConfig().set("joinspawn.pitch", pitch);
         saveConfig();
     }
 
