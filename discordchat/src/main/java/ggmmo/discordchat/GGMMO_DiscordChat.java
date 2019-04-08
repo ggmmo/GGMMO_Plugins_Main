8bd7f0c0242af5b1f3b3b0c0a77463762c2afc85
Jack <jack.w.mccall@gmail.com>
2019-04-08 23:21:57 +1000

    Updated discordchat to v.1.1 (#28)
    * Started modifying DiscordChat to link discord and minecraft accounts
    
    * Discord Chat updated to V 1.1
      - Verification command added

--- discordchat/src/main/java/ggmmo/discordchat/GGMMO_DiscordChat.java
+++ discordchat/src/main/java/ggmmo/discordchat/GGMMO_DiscordChat.java
@@ -1,12 +1,22 @@
 package ggmmo.discordchat;
 
 import ggmmo.discordchat.events.DiscordChatListener;
+import ggmmo.discordchat.utils.MessageManager;
 import org.bukkit.ChatColor;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public class GGMMO_DiscordChat extends JavaPlugin {
     public ConsoleCommandSender console;
+    public File data;
+    public FileConfiguration playerData;
     private DiscordChatListener discordClient;
 
     @Override
@@ -17,9 +27,35 @@ public class GGMMO_DiscordChat extends JavaPlugin {
         // Load configs
         this.getConfig().options().copyDefaults(true);
         this.saveConfig();
+        createDataConfig();
 
         // Start the discord service
         discordClient = new DiscordChatListener(this);
+
+        // Update number of users online (In case of reload
+        discordClient.updateOnlineUsers();
+
+        for (Player player : getServer().getOnlinePlayers()) {
+            // Update verified users
+            if (playerData.contains("Data." + player.getUniqueId().toString())) {
+                discordClient.verifiedMembers.add(player.getUniqueId());
+            }
+        }
+    }
+
+    private void createDataConfig() {
+        data = new File(getDataFolder() + File.separator + "data.yml");
+        if (!data.exists()) {
+            MessageManager.consoleGood("Creating file data.yml");
+            this.saveResource("data.yml", false);
+        }
+
+        playerData = new YamlConfiguration();
+        try {
+            playerData.load(data);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
