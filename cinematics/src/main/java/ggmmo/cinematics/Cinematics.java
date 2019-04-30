b49efa1755692142c5e28352a485d8b8aeb7903a
Jack <jack.w.mccall@gmail.com>
2019-05-01 09:41:11 +1000

    Added weatherWands, title packet command, cinematics plugin (#34)
    - modified gitignore to ignore the target directory from maven builds

---
+++ cinematics/src/main/java/ggmmo/cinematics/Cinematics.java
@@ -0,0 +1,28 @@
+package ggmmo.cinematics;
+
+import ggmmo.cinematics.commands.CmdLightningWand;
+import ggmmo.cinematics.commands.CmdPlayTitle;
+import ggmmo.cinematics.events.EventUseLightningWand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Cinematics extends JavaPlugin {
+    private static Cinematics plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+
+        getCommand("lightningWand").setExecutor(new CmdLightningWand());
+        getCommand("playtitle").setExecutor(new CmdPlayTitle());
+        getServer().getPluginManager().registerEvents(new EventUseLightningWand(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public static Cinematics getPlugin() {
+        return plugin;
+    }
+}
---
+++ cinematics/src/main/java/ggmmo/cinematics/events/EventUseLightningWand.java
@@ -0,0 +1,23 @@
+package ggmmo.cinematics.events;
+
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class EventUseLightningWand implements Listener {
+    @EventHandler
+    public void onUseLightningWand(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        World world = player.getWorld();
+
+        if (player.getInventory() != null) {
+            if (player.getInventory().getItemInMainHand() != null) {
+                String itemName = player.getInventory().getItemInMainHand().getItemMeta().getDisplayName();
+                if (itemName != null && itemName.equals("Lightning Wand"))
+                    world.strikeLightning(player.getTargetBlock(null, 50).getLocation());
+            }
+        }
+    }
+}
