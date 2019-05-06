148820046cc756e77233b84736f6a885519543e6
Jack <jack.w.mccall@gmail.com>
2019-05-06 14:30:04 +1000

    Changed conditions for Lightning wand usage - Only works with right click

--- cinematics/src/main/java/ggmmo/cinematics/events/EventUseLightningWand.java
+++ cinematics/src/main/java/ggmmo/cinematics/events/EventUseLightningWand.java
@@ -4,6 +4,7 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class EventUseLightningWand implements Listener {
@@ -12,7 +13,7 @@ public class EventUseLightningWand implements Listener {
         Player player = event.getPlayer();
         World world = player.getWorld();
 
-        if (player.getInventory() != null) {
+        if ((event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) && player.getInventory() != null) {
             if (player.getInventory().getItemInMainHand() != null) {
                 String itemName = player.getInventory().getItemInMainHand().getItemMeta().getDisplayName();
 
